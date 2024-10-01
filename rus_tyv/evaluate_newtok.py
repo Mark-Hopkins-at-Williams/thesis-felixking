d653b0dc961bbe3c12f42d5423713031a25edb15
Felix King <jfk2@williams.edu>
2024-10-01 09:10:34 -0400

    changes

---
+++ rus_tyv/evaluate_newtok.py
@@ -0,0 +1,61 @@
+# BLEU = 6.15 31.1/10.1/3.9/1.2 (BP = 1.000 ratio = 1.055 hyp_len = 2451 ref_len = 2324)
+# chrF2++ = 31.43
+
+from tqdm import tqdm
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import NllbTokenizer
+
+
+NEW_SPM_PATH = "../../models/nllb-rus-tyv-tokenizer/spm_nllb_tyvan_268k.model"
+
+model_load_name = '/mnt/storage/fking/models/nllb-rus-tyv-v2_newtok'
+model = AutoModelForSeq2SeqLM.from_pretrained(model_load_name).cuda()
+model_tok_name = "facebook/nllb-200-distilled-600M"
+tokenizer = NllbTokenizer.from_pretrained(model_tok_name, vocab_file=NEW_SPM_PATH)
+
+
+
+def translate(
+    text, src_lang='kir_Cyrl', tgt_lang='rus_Cyrl', 
+    a=32, b=3, max_input_length=1024, num_beams=4, **kwargs
+):
+    """Translates a string or a list of strings."""
+    tokenizer.src_lang = src_lang
+    tokenizer.tgt_lang = tgt_lang
+    model.eval() # turn off training mode
+    inputs = tokenizer(
+        text, return_tensors='pt', padding=True, truncation=True, 
+        max_length=max_input_length
+    )
+    result = model.generate(
+        **inputs.to(model.device),
+        forced_bos_token_id=tokenizer.convert_tokens_to_ids(tgt_lang),
+        max_new_tokens=int(a + b * inputs.input_ids.shape[1]),
+        num_beams=num_beams, **kwargs
+    )
+    return tokenizer.batch_decode(result, skip_special_tokens=True)
+    
+
+def batched_translate(texts, batch_size=16, **kwargs):
+    """Translate texts in batches of similar length"""
+    idxs, texts2 = zip(*sorted(enumerate(texts), key=lambda p: len(p[1]), reverse=True))
+    results = []
+    for i in tqdm(range(0, len(texts2), batch_size)):
+        results.extend(translate(texts2[i: i+batch_size], **kwargs))
+    return [p for _, p in sorted(zip(idxs, results))]
+
+import sacrebleu
+import pandas as pd
+bleu_calc = sacrebleu.BLEU()
+chrf_calc = sacrebleu.CHRF(word_order=2)  # this metric is called ChrF++
+
+trans_df = pd.read_csv('/mnt/storage/hopkins/thesis/data/rus_tyv_parallel_50k.tsv', sep="\t")
+
+df_train = trans_df[trans_df.split=='train'].copy() # 49000 items
+df_dev = trans_df[trans_df.split=='dev'].copy()     # 500 items
+df_test = trans_df[trans_df.split=='test'].copy()   # 500 items
+
+rus_translations = batched_translate(df_dev['tyv'].tolist(), src_lang='kir_Cyrl', tgt_lang='rus_Cyrl')
+
+print(bleu_calc.corpus_score(rus_translations, [df_dev['ru'].tolist()]))
+print(chrf_calc.corpus_score(rus_translations, [df_dev['ru'].tolist()]))
