7d51860e3d7642cae0902fbdc7bd15deda0ee621
Felix King <jfk2@williams.edu>
2024-10-02 23:53:48 -0400

    made csvs for all xx-spa pairs in americasnlp

---
+++ xx-spa/train.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import sys
+from transformers.optimization import Adafactor
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+from transformers import get_constant_schedule_with_warmup
+from preprocess import preproc
+import random
+import gc
+import torch
+from tqdm import tqdm
+import numpy as np
+
+model_name = "facebook/nllb-200-distilled-600M"
+batch_size = 16  # 32 already doesn't fit well to 15GB of GPU memory
+max_length = 128  # token sequences will be truncated
+training_steps = 60000  
+train_losses = []  # with this list, I do very simple tracking of average loss
+dev_losses = []  # with this list, I do very simple tracking of average loss
+MODEL_SAVE_PATH = '/mnt/storage/fking/models/nllb-nah-spa-v1' 
+
+
+df_train = get_def_train()
+df_dev = get_def_dev()
+
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+model.cuda()
+optimizer = Adafactor(
+    [p for p in model.parameters() if p.requires_grad],
+    scale_parameter=False,
+    relative_step=False,
+    lr=1e-4,
+    clip_threshold=1.0,
+    weight_decay=1e-3,
+)
+scheduler = get_constant_schedule_with_warmup(optimizer, num_warmup_steps=1000)
+LANGS = [('spa', 'spa_Latn'), ('nah', 'gug_Latn')]
+
+def get_batch_pairs(batch_size, data=df_train):
+    (l1, long1), (l2, long2) = random.sample(LANGS, 2)
+    xx, yy = [], []
+    for _ in range(batch_size):
+        item = data.iloc[random.randint(0, len(data)-1)]
+        xx.append(preproc(item[l1]))
+        yy.append(preproc(item[l2]))
+    return xx, yy, long1, long2
+
+def cleanup():
+    """Try to free GPU memory"""
+    gc.collect()
+    torch.cuda.empty_cache()
+
+
+
+x, y, train_loss = None, None, None
+x_dev, y_dev, dev_loss = None, None, None
+best_dev_loss = None
+cleanup()
+
+for i in tqdm(range(len(train_losses), training_steps)):
+    xx, yy, lang1, lang2 = get_batch_pairs(batch_size)
+    xx_dev, yy_dev, lang1_dev, lang2_dev = get_batch_pairs(batch_size, data=df_dev)
+
+    try:
+        model.train()
+        tokenizer.src_lang = lang1
+        x = tokenizer(xx, return_tensors='pt', padding=True, truncation=True, max_length=max_length).to(model.device)
+        x_dev = tokenizer(xx_dev, return_tensors='pt', padding=True, truncation=True, max_length=max_length).to(model.device)
+        tokenizer.src_lang = lang2
+        y = tokenizer(yy, return_tensors='pt', padding=True, truncation=True, max_length=max_length).to(model.device)
+        y_dev = tokenizer(yy_dev, return_tensors='pt', padding=True, truncation=True, max_length=max_length).to(model.device)
+        # -100 is a magic value ignored in the loss function
+        # because we don't want the model to learn to predict padding ids
+        y.input_ids[y.input_ids == tokenizer.pad_token_id] = -100
+        y_dev.input_ids[y_dev.input_ids == tokenizer.pad_token_id] = -100
+
+        train_loss = model(**x, labels=y.input_ids).loss
+        train_loss.backward()
+        train_losses.append(train_loss.item())
+
+        optimizer.step()
+        optimizer.zero_grad(set_to_none=True)
+        scheduler.step()
+
+        with torch.no_grad():
+            model.eval()
+            dev_loss = model(**x_dev, labels=y_dev.input_ids).loss
+            dev_losses.append(dev_loss.item())
+
+    except RuntimeError as e:  # usually, it is out-of-memory
+        optimizer.zero_grad(set_to_none=True)
+        x, y, train_loss = None, None, None
+        x_dev, y_dev, dev_loss = None, None, None
+        cleanup()
+        print('error', max(len(s) for s in xx + yy), e)
+        continue
+
+    if i % 1000 == 0:
+        # each 1000 steps, I report average loss at these steps
+        print(f'step {i} (train): {np.mean(train_losses[-1000:])}')
+        print(f'step {i} (dev):   {np.mean(dev_losses[-1000:])}')
+        sys.stdout.flush()
+
+    if i % 1000 == 0 and i > 0 and (best_dev_loss is None or dev_loss < best_dev_loss):
+        print("Saving new best model!")
+        model.save_pretrained(MODEL_SAVE_PATH)
+        tokenizer.save_pretrained(MODEL_SAVE_PATH)
+        best_dev_loss = dev_loss
---
+++ xx-spa/utility.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import os
+
+# This program makes csv files containing all the training and dev data for each of the language
+# pairs in the americasnlp dataset. each csv has the headers 
+#               [src_lang_code], spa, split
+
+# where split is either "train" or "dev"
+
+codes = { # these are just the codes that americasnlp uses for their files
+    "ashaninka": "cni",
+    "bribri": "bzd", 
+    "guarani": "gn",   
+    "quechua": "quy",  
+    "aymara": "aym",   
+    "shipibo_konibo": "shp",
+    "chatino": "ctp",
+    "hñähñu": "oto",
+    "nahuatl": "nah",
+    "raramuri": "tar",
+    "wixarika": "hch"  
+    }
+
+parent_path = "/mnt/storage/fking/americasnlp2024/ST1_MachineTranslation/data/"
+            
+
+def get_def_train(path, code):
+    with open(path + '/train.es', 'r') as file:
+        train_es = file.readlines()
+    train_es = [line.strip() for line in train_es]
+    df_tgt_train = pd.DataFrame(train_es, columns=['spa'])
+
+    with open(path + '/train.' + code, 'r') as file:
+        train_src = file.readlines()
+    train_src = [line.strip() for line in train_src]
+    df_src_train = pd.DataFrame(train_src, columns=[code])
+
+    result = pd.concat([df_src_train, df_tgt_train], axis=1)
+    result["split"] = "train"
+    return result
+
+def get_def_dev(path, code):
+    with open(path + '/dev.es', 'r') as file:
+        train_es = file.readlines()
+    train_es = [line.strip() for line in train_es]
+    df_tgt_train = pd.DataFrame(train_es, columns=['spa'])
+
+    with open(path + '/dev.' + code, 'r') as file:
+        train_src = file.readlines()
+    train_src = [line.strip() for line in train_src]
+    df_src_train = pd.DataFrame(train_src, columns=[code])
+
+    result = pd.concat([df_src_train, df_tgt_train], axis=1)
+    result["split"] = "train"
+
+    return result
+
+for subdir, dirs, files in os.walk(parent_path):
+    if subdir == parent_path:
+        for dir_name in dirs:
+            dir_path = os.path.join(subdir, dir_name)
+            language = dir_path.split("/")[-1].split("-")[0] #identify the american language
+
+            train = get_def_train(dir_path, codes[language])
+            dev = get_def_dev(dir_path, codes[language])
+
+            full = pd.concat([train, dev])
+
+            full.to_csv(dir_path + "/all.csv", index = False)
+
+
+
