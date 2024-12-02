6d72a36619a796b9d6e9037ba097b8f7408f76e0
Felix King <felixking@Felixs-MacBook-Air-3.local>
2024-12-02 17:01:02 -0500

    rearranging

--- current/configure.py
+++ current/configure.py
@@ -1,12 +1,14 @@
 USE_CUDA=True
 
 NLLB_SEED_CSV = '/mnt/storage/hopkins/data/nllb/seed/nllb_seed.csv'
+
 NLLB_SEED_LANGS = ['pbt_Arab', 'bho_Deva', 'nus_Latn', 'ban_Latn', 'dzo_Tibt', 'mni_Beng', 'lim_Latn', 
                    'ltg_Latn', 'ace_Latn', 'crh_Latn', 'srd_Latn', 'taq_Latn', 'mri_Latn', 'ary_Arab', 
                    'bam_Latn', 'knc_Arab', 'eng_Latn', 'knc_Latn', 'dik_Latn', 'prs_Arab', 'bjn_Arab', 
                    'vec_Latn', 'fur_Latn', 'kas_Deva', 'kas_Arab', 'arz_Arab', 'lij_Latn', 'ace_Arab', 
                    'bjn_Latn', 'scn_Latn', 'bug_Latn', 'lmo_Latn', 'szl_Latn', 'hne_Deva', 'fuv_Latn', 
                    'taq_Tfng', 'shn_Mymr', 'mag_Deva']
+
 NLLB_SEED_LPS = [(src, 'eng_Latn') for src in NLLB_SEED_LANGS if src != 'eng_Latn']
 
 TEN_SEED_LANGS  = ['pbt_Arab', 'bho_Deva', 'nus_Latn', 'dzo_Tibt', 'mni_Beng', 'lim_Latn', 
